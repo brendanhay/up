a6262fed1868e718037df89aac6e05adf82c100c
brendanhay <brendan.g.hay@gmail.com>
2011-06-28 12:43:19 +1200

    Basic javascript functions to show/hide elements and setup cross-browser xmlhttprequest for progress notification

---
+++ priv/www/scripts.js
@@ -0,0 +1,105 @@
+(function() {
+    var pageLoad = function() {    
+        var timer;
+        var clearTimer = function() {
+            if (undefined !== timer) { 
+                window.clearInterval(timer); 
+            }
+        };
+    
+        var noDependenciesNoProblemCrossBrowserJsonParse = function(response) {
+            return eval(['(', response, ')'].join(''));
+        }
+                        
+        var trackProgress = function() {
+            var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+            var url = ['/progress/', document.getElementById('key').value, '?_method=get'].join(''); 
+            xhr.open('GET', url, true);
+            xhr.setRequestHeader('Content-Type', 'application/javascript'); 
+            xhr.setRequestHeader('Cache-Control', 'no-cache'); 
+            xhr.onerror = function() {
+                hideProgress(false);
+            };
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) { 
+                        var response = noDependenciesNoProblemCrossBrowserJsonParse(xhr.responseText);
+                        updateProgress(response); 
+                    } else {
+                        hideProgress(false);
+                    }
+                }
+            };                           
+            xhr.send(null);                    
+        }
+                   
+        var startProgress = function() {
+            clearTimer();
+            timer = window.setInterval(function() { trackProgress(); }, 250);            
+        }    
+        
+        var progressBar = document.getElementById('progress');
+        var updateProgress = function(response) {  
+            var percent = response.percent;          
+            progressBar.style.width = percent + '%';  
+            if (!(response.id > 0)) {
+                hideProgress(false);
+            } else if (percent > 99) { 
+                hideProgress(response);
+            }
+        }
+        
+        var getFileName = function() {
+            var path = document.getElementById('file').value;
+            var index = (path.indexOf('\\') >= 0 ? path.lastIndexOf('\\') : path.lastIndexOf('/'));
+            var name = path.substring(index);
+            if (name.indexOf('\\') === 0 || name.indexOf('/') === 0) {
+                name = name.substring(1);
+            }        
+            return name;
+        }; 
+                
+        var uploadStatus = document.getElementById('upload_status');
+        var hideProgress = function(response) { 
+            clearTimer();
+            if (!response) {
+                uploadStatus.style.display = 'none';
+                document.getElementById('upload_error').style.display = 'block';
+            } else {
+                window.setTimeout(function() { 
+                    uploadStatus.style.display = 'none';
+                    document.getElementById('upload_success').style.display = 'block';
+                    var showLink = document.getElementById('show_link');
+                    showLink.href = response.path;
+                    showLink.innerText = getFileName();
+                }, 250); 
+            }
+        };
+
+        var uploadButtons = document.getElementById('upload_buttons');
+        var uploadForm = document.getElementById('upload_form');
+        if (uploadForm != null && undefined !== uploadForm) {
+            uploadForm.onsubmit = function() {
+                uploadButtons.style.display = 'none';
+                uploadStatus.style.display = 'block';
+                startProgress();
+                return true;    
+            };
+        }
+    }
+        
+    if (window.attachEvent) {
+        window.attachEvent('onload', pageLoad);
+    } else {
+        if (window.onload) {
+            var onLoad = window.onload;
+            window.onload = function() {
+                onLoad();
+                pageLoad();
+            };
+        } else {
+            window.onload = pageLoad;
+        }
+    }    
+})();
+
